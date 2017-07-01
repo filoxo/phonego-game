9190fd90a952f0f22ce09b0c82fed267044c1551
Carlos Filoteo <c.filoteo@live.com>
2017-06-30 23:01:48 -0600

    Add form to Home screen

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,11 +2,40 @@ import { h, Component } from 'preact'
 import style from './style'
 
 export default class Home extends Component {
+	constructor(props) {
+		super(props)
+		this.state = { value: '' }
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+	handleChange(event) {
+		this.setState({ value: event.target.value })
+	}
+	handleSubmit(event) {
+		alert('A name was submitted: ' + this.state.value)
+		event.preventDefault()
+	}
 	render() {
 		return (
 			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Home component.</p>
+				<h2>Welcome!</h2>
+				<form onSubmit={this.handleSubmit}>
+					<div>
+						<input
+							type="text"
+							name="name"
+							placeholder="Enter your name"
+							value={this.state.value}
+							onChange={this.handleChange}
+						/>
+					</div>
+					<hr />
+					<div>
+						<input type="text" name="name" placeholder="Enter Room Code" />
+					</div>
+					<div>&mdash;&nbsp;or&nbsp;&mdash;</div>
+					<button type="submit">Start a new game</button>
+				</form>
 			</div>
 		)
 	}
