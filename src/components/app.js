0ec3213cf943d127152d7dfd67c93c763fbac7ec
Carlos Filoteo <c.filoteo@live.com>
2017-06-30 21:22:28 -0600

    Import firebase

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import { Router } from 'preact-router';
 import Header from './header';
 import Home from '../routes/home';
 import Profile from '../routes/profile';
+import firebase from '../lib/firebase.js';
 // import Home from 'async!./home';
 // import Profile from 'async!./profile';
 
