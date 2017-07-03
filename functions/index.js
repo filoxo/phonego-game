d2e6284352057d32b9ed555e20053d3046d6d5d6
Carlos Filoteo <c.filoteo@live.com>
2017-07-03 16:06:15 -0600

    Add firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
-const functions = require('firebase-functions')
+const functions = require('firebase-functions'),
+	sortid = require('shortid32'),
+	admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase)
+
+exports.addShortGameId = functions.database
+	.ref('/games/{pushId}')
+	.onWrite(event => {
+		if (event.data.previous.exists()) return
+		return event.data.ref.child('sid').set(shortid.generate())
+	})
