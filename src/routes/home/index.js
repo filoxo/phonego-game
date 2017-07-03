6d03e758937624dfb3150341944af05c3c7b3eb9
Carlos Filoteo <c.filoteo@live.com>
2017-07-02 21:42:59 -0600

    Authenticate on submit and set displayName

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,5 +1,7 @@
 import { h, Component } from 'preact'
 import style from './style'
+import firebase from 'firebase'
+import 'firebase/auth'
 
 export default class Home extends Component {
 	constructor(props) {
@@ -12,24 +14,31 @@ export default class Home extends Component {
 		this.setState({ value: event.target.value })
 	}
 	handleSubmit(event) {
-		alert('A name was submitted: ' + this.state.value)
 		event.preventDefault()
+		firebase
+			.auth()
+			.signInAnonymously()
+			.then(user => {
+				user.updateProfile({ displayName: this.state.value })
+			})
+			.catch(console.log)
 	}
 	render() {
 		return (
 			<div class={style.home}>
 				<h2>Welcome!</h2>
 				<form onSubmit={this.handleSubmit}>
-					<div>
-						<input
-							type="text"
-							name="name"
-							placeholder="Enter your name"
-							value={this.state.value}
-							onChange={this.handleChange}
-						/>
-					</div>
-					<hr />
+					{this.props.username === null &&
+						<div>
+							<input
+								type="text"
+								name="name"
+								placeholder="Enter your name"
+								value={this.state.value}
+								onChange={this.handleChange}
+							/>
+							<hr />
+						</div>}
 					<div>
 						<input type="text" name="name" placeholder="Enter Room Code" />
 					</div>
